c2a41399afe18136cd79ddfafa445a8cf88da4b7
Alexey3934 <gorrus39@mial.ru>
2023-03-24 13:28:48 +0200

    q

--- adminka/config/environments/development.rb
+++ adminka/config/environments/development.rb
@@ -8,7 +8,7 @@ Rails.application.configure do
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
 
-  # config.hosts << "77.91.84.82"
+  config.hosts << "77.91.84.82"
 
   # Do not eager load code on boot.
   config.eager_load = false
@@ -69,5 +69,4 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
-  config.action_cable.disable_request_forgery_protection = false
 end
