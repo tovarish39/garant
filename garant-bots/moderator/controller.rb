4ba28d0c3088ac42447d4fd57a8e26cfc5cd1ac6
Alexey3934 <gorrus39@mial.ru>
2023-06-02 22:24:58 +0200

    q

--- garant-bots/moderator/controller.rb
+++ garant-bots/moderator/controller.rb
@@ -9,24 +9,24 @@ def handle
   begin
     
   
-  if    $mod.username == '-'
-    require_username # обязательный юзернейм
-    # elsif $mes.instance_of?(ChatMemberUpdated) # реагирует только от private chat
-  elsif ($mes.class == CallbackClass) || ($mes.class == MessageClass)
-    $chat_id = ($mes.class == MessageClass) ? $mes.chat.id : $mes.message.chat.id # после UpdatedChatMember
-    # from_state = :moderate           # предидущее состояние
-    from_state = $mod.aasm_state.to_sym           # предидущее состояние
-    event_bot = StateMachine.new
+    if    $mod.username == '-'
+      require_username # обязательный юзернейм
+    elsif $mes.instance_of?(ChatMemberUpdated) # реагирует только от private chat
+    elsif ($mes.class == CallbackClass) || ($mes.class == MessageClass)
+      $chat_id = ($mes.class == MessageClass) ? $mes.chat.id : $mes.message.chat.id # после UpdatedChatMember
+      # from_state = :moderate           # предидущее состояние
+      from_state = $mod.aasm_state.to_sym           # предидущее состояние
+      event_bot = StateMachine.new
 
-    event_bot.aasm.current_state = from_state
-    event_bot.method("#{from_state}_action").call # event = #{from_state}_action
+      event_bot.aasm.current_state = from_state
+      event_bot.method("#{from_state}_action").call # event = #{from_state}_action
 
-    new_state = event_bot.aasm.current_state
-    $mod.update(aasm_state: new_state)            # запись нового состояния
+      new_state = event_bot.aasm.current_state
+      $mod.update(aasm_state: new_state)            # запись нового состояния
+    end
+  rescue => exception
+
   end
-rescue => exception
-    
-end
 
 end
 
